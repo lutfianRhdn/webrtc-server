c94e4b8aa4fa5dbfce1c8c9f11bc7aa00e10fcb6
Lutfian Rhdn <lutfian.rhdn@gmail.com>
2023-09-14 01:35:10 +0700

    chagne port

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require("http")
 const Socket = require("websocket").server
 const server = http.createServer(()=>{})
 
-server.listen(3000,()=>{
-        console.log("Server started on port: 3000")
+server.listen(8080,()=>{
+        console.log("Server started on port: 8080")
 })
 
 const webSocket = new Socket({httpServer:server})
